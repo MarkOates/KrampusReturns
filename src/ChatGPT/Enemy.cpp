b84b7a46b934f3d424e28d9fc17dbb53869ae744
Mark Oates <markoates0@gmail.com>
2022-12-26 10:56:59 -0500

    Remove unused state switch statement

--- src/ChatGPT/Enemy.cpp
+++ src/ChatGPT/Enemy.cpp
@@ -22,26 +22,6 @@ Enemy::state_t Enemy::get_state() const { return state_; }
 AllegroFlare::Prototypes::Platforming2D::Entities::MovementStrategies2D::Base* Enemy::get_movement_strategy() const { return movement_strategy_; }
 
 void Enemy::set_state(state_t state) {
-    switch (state) {
-        case state_t::SPAWNING:
-            // TODO: add code to handle the SPAWNING state
-            break;
-        case state_t::ATTACKING:
-            // TODO: add code to handle the ATTACKING state
-            break;
-        case state_t::WANDERING:
-            // TODO: add code to handle the WANDERING state
-            break;
-        case state_t::TAKING_DAMAGE:
-            // TODO: add code to handle the TAKING_DAMAGE state
-            break;
-        case state_t::DYING:
-            // TODO: add code to handle the DYING state
-            break;
-        case state_t::DEAD:
-            // TODO: add code to handle the DEAD state
-            break;
-    }
     state_ = state;
 }
 
@@ -50,26 +30,10 @@ void Enemy::set_movement_strategy(AllegroFlare::Prototypes::Platforming2D::Entit
 void Enemy::update() {
     if (movement_strategy_) movement_strategy_->update();
     FrameAnimated2D::update();
-    switch (state_) {
-        case state_t::SPAWNING:
-            // TODO: add custom code related to the SPAWNING state
-            break;
-        case state_t::ATTACKING:
-            // TODO: add custom code related to the ATTACKING state
-            break;
-        case state_t::WANDERING:
-            // TODO: add custom code related to the WANDERING state
-            break;
-        case state_t::TAKING_DAMAGE:
-            // TODO: add custom code related to the TAKING_DAMAGE state
-            break;
-        case state_t::DYING:
-            // TODO: add custom code related to the DYING state
-            break;
-        case state_t::DEAD:
-            // TODO: add custom code related to the DEAD state
-            break;
-    }
 }
 
 } // namespace ChatGPT
+
+
+
+
